976810d87974b2bde6f7047d56a7feea3c49c98c
quyen <vanquyen101112@gmail.com>
2022-01-05 20:42:54 +0700

    change index.js on config

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -9,12 +9,17 @@ const mongoose = require('mongoose');
         console.log('connect failure!!!');
     }
 } */
-function connect(){
-    const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hotel_dev ';
-    mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
-        .then(() => console.log('MongoDB connected!'))
-        .catch(err => console.log('connect failure'));
 
+
+async function connect(){
+    const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hotel_dev ';
+    try{
+        await mongoose.connect(uri);
+        console.log('connect succesfully!!!');
+    }
+    catch (error){
+        console.log('connect failure!!!'+ error);
+    }
 }
 
 module.exports = {connect}
