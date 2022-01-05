3b05bf8486f5661ec07cd4d66fffe2033ede72a5
quyen <vanquyen101112@gmail.com>
2022-01-05 20:04:50 +0700

    add database atlast to heroku

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
- async function connect(){
+/*  async function connect(){
     try{
         await mongoose.connect('mongodb://127.0.0.1:27017/hotel_dev');
         console.log('connect succesfully!!!');
@@ -8,6 +8,10 @@ const mongoose = require('mongoose');
     catch (error){
         console.log('connect failure!!!');
     }
-}
+} */
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hotel_dev ';
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log('MongoDB connected!'))
+    .catch(err => console.log('connect failure'));
 
 module.exports = {connect}
